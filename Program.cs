6e86f6faccb503196aa28de64adc20ad06bd8337
shlomo <s0556732899@gmail.com>
2025-05-14 15:41:18 +0300

    conversion

--- Program.cs
+++ Program.cs
@@ -14,10 +14,34 @@ public class AtBash
                 count++;
         return count;
     }
+    static string decrypt(string code)
+    {
+        string code_oflower = code.ToLower();
+        string decryp_kod = "";
+        string small_abc = "abcdefghijklmnopqrstuvwxyz";
+        foreach (char c in code_oflower)
+        {
+            if (!char.IsLetter(c))
+            {
+                decryp_kod += c;
+            }
+            else
+            {
+                decryp_kod += small_abc[small_abc.Length - 1 - small_abc.IndexOf(c)];
+            }
+        }
+        
+        return decryp_kod;
+    }
+
     public static void Main(string[] args)
     {
-        string a = "Hello World Hello World Hello World";
-        string [] b = new string [] { "Hello" };
-        Console.WriteLine(DangerCheck(a,b));
+        //string a = "Hello World Hello World Hello World";
+        //string [] b = new string [] { "Hello" };
+        //Console.WriteLine(DangerCheck(a,b));
+        
+        Console.WriteLine(decrypt("Lfi ulixvh ziv kivkzirmt uli z nzqli zggzxp lm gsv Arlmrhg vmvnb.\r\nGsv ilxpvg fmrgh ziv ivzwb zmw dzrgrmt uli gsv hrtmzo.\r\nYlnyh szev yvvm kozxvw mvzi pvb olxzgrlmh.\r\nMfpsyz urtsgvih ziv hgzmwrmt yb uli tilfmw rmurogizgrlm.\r\nGsv zggzxp droo yv hfwwvm zmw hgilmt -- gsvb dlm’g hvv rg xlnrmt.\r\nDv nfhg hgzb srwwvm zmw pvvk gsv kozm hvxivg fmgro gsv ozhg nlnvmg.\r\nErxglib rh mvzi. Hgzb ivzwb.\r\n"));
+
     }
 }
+
