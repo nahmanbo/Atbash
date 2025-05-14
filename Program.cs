37f386c88f7202dff4fc9e06f2100e47e7b6a0a2
nahmanbo <nhmanbo2003@gmail.com>
2025-05-14 15:46:11 +0300

    func print alert

--- Program.cs
+++ Program.cs
@@ -5,14 +5,30 @@ using System;
 
 public class AtBash
 {
-    static int DangerCheck(string message, string[] dangerWords)
+    static (int, string) DangerCheck(string message, string[] dangerWords)
     {
         int count = 0;
         string[] messageArray = message.Split(" ");
         foreach (string word in messageArray)
             if (dangerWords.Contains(word))
                 count++;
-        return count;
+        return (count, message);
+    }
+
+    static void PrintAlert(int dangerLevel, string message)
+    {
+        string finalMessage = "";
+        if (dangerLevel >= 1 && dangerLevel <= 5)
+            finalMessage += "WARNING!!!\n\n";
+        else if (dangerLevel >= 6 && dangerLevel <= 10)
+            finalMessage += "DANGER!!!\n\n";
+        else if (dangerLevel >= 11 && dangerLevel <= 15)
+            finalMessage += "ULTRA ALERT!!!\n\n";
+        else 
+            finalMessage += "The hazard level check was not completed properly.!!!\n\n";
+        
+        finalMessage += message + "\n\n";
+        finalMessage += $"danger level is {dangerLevel}";
     }
     public static void Main(string[] args)
     {
